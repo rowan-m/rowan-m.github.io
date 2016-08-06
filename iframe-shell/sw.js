83efedcb8240e547dd6aa098e23c5c2ab1a1d93a
Rowan Merewood <rowan@merewood.org>
2016-08-06 20:59:38 +0100

    Cache app shell

--- iframe-shell/sw.js
+++ iframe-shell/sw.js
@@ -1,3 +1,34 @@
+var cacheName = 'fragile-shell-v20160806';
+var filesToCache = [
+    '/iframe-shell/',
+    '/iframe-shell/index.html',
+    '/iframe-shell/placeholder.html',
+    '/iframe-shell/amp/01.amp.html',
+    '/iframe-shell/amp/02.amp.html',
+    '/iframe-shell/amp/03.amp.html',
+    '/iframe-shell/amp/04.amp.html',
+];
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
 self.addEventListener('fetch', event => {
     suffixIndex = event.request.url.indexOf('.amp.html');
     isEmbed = event.request.url.endsWith('?embed=1');
@@ -5,6 +36,12 @@ self.addEventListener('fetch', event => {
     if (!isEmbed && suffixIndex !== -1) {
         event.respondWith(
             fetch('/iframe-shell/')
+        );
+    } else {
+        e.respondWith(
+            caches.match(e.request).then(function(response) {
+                return response || fetch(e.request);
+            })
         );
     }
 });
