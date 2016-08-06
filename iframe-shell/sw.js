6ae43548326c43bc1f938858c72e7ad548667376
Rowan Merewood <rowan@merewood.org>
2016-08-06 21:27:04 +0100

    Fix copy paste errors

--- iframe-shell/sw.js
+++ iframe-shell/sw.js
@@ -6,30 +6,35 @@ var filesToCache = [
     '/iframe-shell/amp/01.amp.html',
     '/iframe-shell/amp/02.amp.html',
     '/iframe-shell/amp/03.amp.html',
-    '/iframe-shell/amp/04.amp.html',
+    '/iframe-shell/amp/04.amp.html'
 ];
 
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
+self.addEventListener('install', function(event) {
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
-self.addEventListener('activate', function(e) {
-    e.waitUntil(
-        caches.keys().then(function(keyList) {
-            return Promise.all(keyList.map(function(key) {
-                if (key !== cacheName) {
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
+self.addEventListener('activate', function(event) {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
+  console.log('[ServiceWorker] Fetch request', event.request.url);
     suffixIndex = event.request.url.indexOf('.amp.html');
     isEmbed = event.request.url.endsWith('?embed=1');
 
@@ -38,9 +43,9 @@ self.addEventListener('fetch', event => {
             fetch('/iframe-shell/')
         );
     } else {
-        e.respondWith(
-            caches.match(e.request).then(function(response) {
-                return response || fetch(e.request);
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                return response || fetch(event.request);
             })
         );
     }
