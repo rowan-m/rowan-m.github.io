d180e7b9f1b4fd92be2e08d274fa4ab9c71e1fb5
Rowan Merewood <rowan@merewood.org>
2016-08-07 16:37:03 +0100

    Actually respond offline

--- iframe-shell/sw.js
+++ iframe-shell/sw.js
@@ -3,46 +3,34 @@ var filesToCache = [
     '/iframe-shell/',
     '/iframe-shell/index.html',
     '/iframe-shell/placeholder.html',
-    '/iframe-shell/amp/01.amp.html',
-    '/iframe-shell/amp/02.amp.html',
-    '/iframe-shell/amp/03.amp.html',
-    '/iframe-shell/amp/04.amp.html'
+    '/iframe-shell/amp/01.amp.html?embed=1',
+    '/iframe-shell/amp/02.amp.html?embed=1',
+    '/iframe-shell/amp/03.amp.html?embed=1',
+    '/iframe-shell/amp/04.amp.html?embed=1'
 ];
 
 self.addEventListener('install', function(event) {
-  console.log('[ServiceWorker] Install');
-  event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('[ServiceWorker] Activate');
-  event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName && key !== dataCacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  console.log('[ServiceWorker] Fetch request', event.request.url);
+self.addEventListener('fetch', function(event) {
+    console.log('[ServiceWorker] Fetch request', event.request.url);
     suffixIndex = event.request.url.indexOf('.amp.html');
     isEmbed = event.request.url.endsWith('?embed=1');
 
     if (!isEmbed && suffixIndex !== -1) {
+        console.log('[ServiceWorker] Load iframe-shell ', event.request.url);
         event.respondWith(
-            fetch('/iframe-shell/')
+            caches.match(new Request('/iframe-shell/index.html')).then(function(response) {
+                return response || fetch('/iframe-shell/index.html');
+            })
         );
     } else {
+        console.log('[ServiceWorker] Check cache ', event.request.url);
         event.respondWith(
             caches.match(event.request).then(function(response) {
                 return response || fetch(event.request);
