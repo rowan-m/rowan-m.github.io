02a24f2e251466fb986d0f90d7df0a8bd406f358
Rowan Merewood <rowan@merewood.org>
2016-08-07 17:20:37 +0100

    Stale while revalidate caching

--- iframe-shell/sw.js
+++ iframe-shell/sw.js
@@ -1,39 +1,41 @@
 var cacheName = 'fragile-shell-v20160806';
-var filesToCache = [
-    '/iframe-shell/',
-    '/iframe-shell/index.html',
-    '/iframe-shell/placeholder.html',
-    '/iframe-shell/amp/01.amp.html?embed=1',
-    '/iframe-shell/amp/02.amp.html?embed=1',
-    '/iframe-shell/amp/03.amp.html?embed=1',
-    '/iframe-shell/amp/04.amp.html?embed=1'
-];
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache);
+            return cache.addAll([
+                '/iframe-shell/',
+                '/iframe-shell/index.html',
+                '/iframe-shell/placeholder.html',
+                '/iframe-shell/amp/01.amp.html?embed=1',
+                '/iframe-shell/amp/02.amp.html?embed=1',
+                '/iframe-shell/amp/03.amp.html?embed=1',
+                '/iframe-shell/amp/04.amp.html?embed=1',
+            ]);
         })
     );
 });
 
 self.addEventListener('fetch', function(event) {
-    console.log('[ServiceWorker] Fetch request', event.request.url);
     suffixIndex = event.request.url.indexOf('.amp.html');
     isEmbed = event.request.url.endsWith('?embed=1');
 
     if (!isEmbed && suffixIndex !== -1) {
-        console.log('[ServiceWorker] Load iframe-shell ', event.request.url);
         event.respondWith(
             caches.match(new Request('/iframe-shell/index.html')).then(function(response) {
                 return response || fetch('/iframe-shell/index.html');
             })
         );
     } else {
-        console.log('[ServiceWorker] Check cache ', event.request.url);
         event.respondWith(
-            caches.match(event.request).then(function(response) {
-                return response || fetch(event.request);
+            caches.open(cacheName).then(function(cache) {
+                return cache.match(event.request).then(function(response) {
+                    var fetchPromise = fetch(event.request).then(function(networkResponse) {
+                        cache.put(event.request, networkResponse.clone());
+                        return networkResponse;
+                    })
+                    return response || fetchPromise;
+                })
             })
         );
     }
