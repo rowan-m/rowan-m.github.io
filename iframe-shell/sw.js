b9632bfec10df2c519f6d9c7f65f2ea8418da094
Rowan Merewood <merewood@google.com>
2016-08-08 13:42:57 +0100

    Add explanatory comments

--- iframe-shell/sw.js
+++ iframe-shell/sw.js
@@ -1,5 +1,6 @@
 var cacheName = 'fragile-shell-v20160806';
 
+// Cache a very basic selection of resources
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
@@ -11,8 +12,6 @@ self.addEventListener('install', function(event) {
         '/iframe-shell/amp/02.amp.html?embed=1',
         '/iframe-shell/img/shell.jpg',
         '/iframe-shell/img/shell02.jpg',
-        '/iframe-shell/img/icons/favicon-16x16.png',
-        '/iframe-shell/img/icons/favicon-96x96.png',
       ]);
     })
   );
@@ -22,13 +21,18 @@ self.addEventListener('fetch', function(event) {
   suffixIndex = event.request.url.indexOf('.amp.html');
   isEmbed = event.request.url.endsWith('?embed=1');
 
+  // If the requested URL contains "amp.html" and is not an embed,
+  // i.e. the AMP page has been requested directly
   if (!isEmbed && suffixIndex !== -1) {
     event.respondWith(
       caches.match(new Request('/iframe-shell/index.html')).then(function(response) {
+        // Then we need to serve the shell instead
         return response || fetch('/iframe-shell/index.html');
       })
     );
   } else {
+    // Otherwise, we have some very simple functionality to check the cache
+    // and serve an offline page when not connected.
     event.respondWith(
       caches.match(event.request).then(function(response) {
         return response || fetch(event.request);
